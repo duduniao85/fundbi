4b63ac2308b960abd00722d967f1e6175b843570
xuyuming <clark_xym@163.com>
2016-06-08 16:02:47 +0800

    新建一个典型公募基金的固定报表项目，该项目主要涉及基本保有量分析报表，交易量分析报表，等等 20160608 增加ORACLE的MODEL层映射

--- StaticReport/models.py
+++ StaticReport/models.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+##############################连接ORACLE数据库###############################
+##############################取数据表数据###################################
+class table_fundshare():
+    pass
+
+
+
--- StaticReport/views.py
+++ StaticReport/views.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
+#############################################define data generators start #################################3
+
+
+series_Shares=[]
+series_Trade=[]
+
+
+
+#整理出DATAFRAME与SERIES的关系，进行图表绘制，图表的绘制基本是根据SERIS来获取
+
+##############################################define route view functions start################################
+
+def chart2(request):
+    context = {
+        '':series_Shares
+    }
